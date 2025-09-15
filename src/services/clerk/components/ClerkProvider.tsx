84f339e54b47af15746c17f35ea649dc6176651d
KoTerryHtay <luaye1990@gmail.com>
2025-09-15 15:58:44 +0630

    Clerk Theming

--- src/services/clerk/components/ClerkProvider.tsx
+++ src/services/clerk/components/ClerkProvider.tsx
@@ -5,5 +5,33 @@ export default function ClerkProvider({
 }: {
   children: React.ReactNode;
 }) {
-  return <OriginalClerkProvider>{children}</OriginalClerkProvider>;
+  return (
+    <OriginalClerkProvider
+      appearance={{
+        cssLayerName: "vendor",
+        variables: {
+          colorBackground: "var(--color-background)",
+          borderRadius: "var(--radius-md)",
+          colorBorder: "var(--color-secondary-foreground)",
+          colorDanger: "var(--color-destructive)",
+          colorForeground: "var(--color-foreground)",
+          colorPrimary: "var(--color-primary)",
+          colorPrimaryForeground: "var(--color-primary-foreground)",
+          colorInput: "var(--color-input)",
+          colorInputForeground: "var(--color-text)",
+          colorMuted: "var(--color-muted)",
+          colorMutedForeground: "var(--color-muted-foreground)",
+          colorNeutral: "var(--color-secondary-foreground)",
+          colorRing: "var(--color-ring)",
+          colorShadow: "var(--color-shadow-color)",
+          colorSuccess: "var(--color-primary)",
+          colorWarning: "var(--color-warning)",
+          fontFamily: "var(--font-sans)",
+          fontFamilyButtons: "var(--font-sans)",
+        },
+      }}
+    >
+      {children}
+    </OriginalClerkProvider>
+  );
 }
